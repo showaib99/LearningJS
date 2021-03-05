1d9aaaa23743812af14683766bccc52d66916400
showaib99 <showaib99@gmail.com>
2021-03-05 12:30:44 +0600

    6 & 9

---
+++ 6_What_is_Variables/variable.js
@@ -0,0 +1,12 @@
+var name1 = 'S M Showaib Alam'
+var age = 28
+
+// console.log(name1)
+// console.log(age)
+
+
+
+console.log(name1 + 'Showaib Alam knows Javascript')
+console.log('His age is only ' + age)
+console.log(name1 + 'Showaib Alam is creating a Javascript course for all')
+console.log('But his age is only ' + age)
---
+++ 9_JS_Basic_Numbers/basic_numbers.js
@@ -0,0 +1,10 @@
+var a = 15
+var b = 3.14
+var c = Number('45.5')
+console.log(c)
+console.log(Number.parseFloat(c))
+console.log(Number.parseInt(c))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(1/0) // Infinity
+console.log('abd' * 10) // NaN = Not a Number
