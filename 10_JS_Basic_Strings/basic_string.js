eb8127973caf61d6a7d2a409d8403e61c06bf402
showaib99 <showaib99@gmail.com>
2021-03-05 12:43:16 +0600

    10

---
+++ 10_JS_Basic_Strings/basic_string.js
@@ -0,0 +1,10 @@
+var str = 'How are you?'
+var str1 = "I am fine."
+var str2 = `And you?` // ES6
+console.log(str, str1, str2)
+
+
+var str3 = String(452)
+var str4 = String('Everything')
+var str5 = String(3.1416)
+console.log(str3, str4, str5)
